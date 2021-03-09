3f46018f38c2e90254200bf12dae506066521a7d
xd-elon <alandevelope@gmail.com>
2021-03-09 14:39:22 -0300

    atualização services

--- src/services/CreateAppointmentServices.ts
+++ src/services/CreateAppointmentServices.ts
@@ -1,3 +1,35 @@
-class CreateAppointments {
-    
+import { startOfHour } from 'date-fns';
+import Appointment from '../models/Appointment';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+interface RequestDTO {
+    provider: string;
+    date: Date
 }
+
+class CreateAppointments {
+  private appointmentsRepository: AppointmentsRepository;
+
+  constructor(appointmentsRepository: AppointmentsRepository) {
+        this.appointmentsRepository = appointmentsRepository;
+  }
+
+  public execute({provider, date}: RequestDTO): Appointment {
+    const appointmentDate = startOfHour(date);
+    
+    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(appointmentDate);
+
+    if(findAppointmentInSameDate){
+        throw Error('this appointment is already booked')
+    }
+    
+    const appointment  = this.appointmentsRepository.create({
+        provider, 
+        date: appointmentDate,
+    });
+
+    return appointment;
+    }
+}
+
+export default CreateAppointments;
