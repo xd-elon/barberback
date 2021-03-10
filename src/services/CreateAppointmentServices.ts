34197297203ba869e1e04c7af74ea526c17347f0
xd-elon <alandevelope@gmail.com>
2021-03-10 18:31:11 -0300

    atualização services

--- src/services/CreateAppointmentServices.ts
+++ src/services/CreateAppointmentServices.ts
@@ -1,4 +1,6 @@
 import { startOfHour } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
 import Appointment from '../models/Appointment';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 
@@ -8,25 +10,25 @@ interface RequestDTO {
 }
 
 class CreateAppointments {
-  private appointmentsRepository: AppointmentsRepository;
+  public async execute({provider, date}: RequestDTO): Promise<Appointment> {
+    const appointmentsRepository = getCustomRepository(AppointmentsRepository)
 
-  constructor(appointmentsRepository: AppointmentsRepository) {
-        this.appointmentsRepository = appointmentsRepository;
-  }
-
-  public execute({provider, date}: RequestDTO): Appointment {
     const appointmentDate = startOfHour(date);
     
-    const findAppointmentInSameDate = this.appointmentsRepository.findByDate(appointmentDate);
+    const findAppointmentInSameDate = appointmentsRepository.findByDate(
+        appointmentDate
+    );
 
     if(findAppointmentInSameDate){
         throw Error('this appointment is already booked')
     }
     
-    const appointment  = this.appointmentsRepository.create({
+    const appointment  = appointmentsRepository.create({
         provider, 
         date: appointmentDate,
     });
+
+    await appointmentsRepository.save(appointment)
 
     return appointment;
     }
