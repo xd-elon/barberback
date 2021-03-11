f8b4338ab23759013db8de765febe470f67fcf80
xd-elon <alandevelope@gmail.com>
2021-03-11 17:27:00 -0300

    Repository

--- src/repositories/AppointmentsRepository.ts
+++ src/repositories/AppointmentsRepository.ts
@@ -4,7 +4,6 @@ import Appointment from '../models/Appointment';
 @EntityRepository(Appointment)
 class AppointmentsRepository extends Repository<Appointment> {
     public async findByDate(date: Date): Promise<Appointment | null> {
-
         const findAppointment =  await this.findOne({
             where: {date},
         });
