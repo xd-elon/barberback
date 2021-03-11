46f4615bf1ad20e67aac9e8b57d81bab6d6b74c2
xd-elon <alandevelope@gmail.com>
2021-03-11 17:24:04 -0300

    migrações provider, usuario e alteração de coluna

--- src/database/migrations/1615464090707-CreateUsers.ts
+++ src/database/migrations/1615464090707-CreateUsers.ts
@@ -1,10 +1,10 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export default class CreateAppointments1615387459176 implements MigrationInterface {
+export default class CreateUsers1615464090707 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
           new Table({
-            name: 'appointments',
+            name: 'users',
             columns: [
                 {
                     name: 'id',
@@ -14,14 +14,17 @@ export default class CreateAppointments1615387459176 implements MigrationInterfa
                     default: 'uuid_generate_v4()',
                 },
                 {
-                    name: 'provider',
+                    name: 'name',
                     type: 'varchar',
-                    isNullable: false,
                 },
                 {
-                    name: 'date',
-                    type: 'timestamp with time zone',
-                    isNullable: false,
+                    name: 'email',
+                    type: 'varchar',
+                    isUnique: true
+                },
+                {
+                    name: 'password',
+                    type: 'varchar',
                 },
                 {
                     name: 'created_at',
@@ -32,14 +35,14 @@ export default class CreateAppointments1615387459176 implements MigrationInterfa
                     name: 'updated_at',
                     type: 'timestamp',
                     default: 'now()',
-                },
-            ],
+                }
+            ]
           })
         );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments')
+        await queryRunner.dropTable('users')
     }
 
 }
