d95cf5b971a11c4225ad7fc621ddc341fdb893cc
xd-elon <alandevelope@gmail.com>
2021-03-16 15:03:43 -0300

    update upload

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,9 +2,14 @@ import path from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
 
+
+const tmpfolder = path.resolve(__dirname, '..', '..', 'tmp');
 export default {
+
+    directory: tmpfolder,
+
     storage: multer.diskStorage({
-        destination: path.resolve(__dirname, '..', '..', 'tmp'),
+        destination: tmpfolder,
         filename(request, file, callback) {
             const fileHash = crypto.randomBytes(10).toString('hex');
             const fileName = `${fileHash}-${file.originalname}`;
