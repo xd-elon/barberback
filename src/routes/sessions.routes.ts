1db2a2ebd401c9d690ad1e764255ee88d57cc704
xd-elon <alandevelope@gmail.com>
2021-03-16 15:07:41 -0300

    update routes

--- src/routes/sessions.routes.ts
+++ src/routes/sessions.routes.ts
@@ -5,7 +5,7 @@ import AuthenticateUserService from '../services/AuthenticateUserService';
 const sessionsRouter = Router();
 
 sessionsRouter.post('/', async (request, response) => {
-  try {
+  
     const { email, password } = request.body;
 
     const authenticateUser = new AuthenticateUserService();
@@ -16,9 +16,6 @@ sessionsRouter.post('/', async (request, response) => {
     });
 
     return response.json({ user, token });
-  } catch(err) {
-     return response.status(400).json({ error: err.message });
-  }
 });
 
 export default sessionsRouter;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,11 +1,17 @@
-import { Router } from 'express';
+import {request, Router} from 'express';
+import multer from 'multer';
+import uploadConfig from '../config/upload';
 
 import CreateUseService from '../services/CreateUserService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-  try {
+
     const {name, email, password} = request.body;
 
     const createUser = new CreateUseService();
@@ -19,9 +25,22 @@ usersRouter.post('/', async (request, response) => {
     //delete user.password;
 
     return response.send(user);
-  } catch(err) {
-     return response.status(400).json({ error: err.message });
-  }
+});
+
+usersRouter.patch(
+  '/avatar',
+ensureAuthenticated,
+upload.single('avatar'),
+async (request, response) => {
+    const updateUserAvatar = new UpdateUserAvatarService();
+
+    const user = await updateUserAvatar.execute({
+      user_id: request.user.id,
+      filename: request.file.filename,
+    });
+    
+
+    return response.json(user)
 });
 
 export default usersRouter;
