b8c8e3bc9abff0d740c7d62708439e383c15387c
xd-elon <alandevelope@gmail.com>
2021-03-20 20:15:20 -0300

    entry points

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -9,7 +9,7 @@ import routes from './routes'
 import AppError from '@shared/errors/AppError';
 
 import '@shared/infra/typeorm';
-
+import '@shared/container';
 const app = express();
 
 app.use(cors());
