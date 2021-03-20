e829d634a15eca88981fdda577d47c1e52a9bf26
xd-elon <alandevelope@gmail.com>
2021-03-20 20:04:57 -0300

    users routes

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,46 +1,29 @@
 import {Router} from 'express';
 import multer from 'multer';
+import { container } from 'tsyringe';
+
 import uploadConfig from '@config/upload';
 
-import CreateUseService from '@modules/users/services/CreateUserService';
+import UsersController from '../controller/UsersController';
+import UserAvatarController from '../controller/UserAvatarController';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
-import UpdateUserAvatarService from '@modules/users/services/UpdateUserAvatarService';
+
 
 const usersRouter = Router();
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', async (request, response) => {
 
-    const {name, email, password} = request.body;
-
-    const createUser = new CreateUseService();
-
-    const user = await  createUser.execute({
-      name,
-      email,
-      password,
-    })
-
-    //delete user.password;
-
-    return response.send(user);
-});
+usersRouter.post('/', usersController.create);
 
 usersRouter.patch(
-  '/avatar',
-ensureAuthenticated,
-upload.single('avatar'),
-async (request, response) => {
-    const updateUserAvatar = new UpdateUserAvatarService();
-
-    const user = await updateUserAvatar.execute({
-      user_id: request.user.id,
-      filename: request.file.filename,
-    });
-    
-
-    return response.json(user)
-});
+    '/avatar',
+    ensureAuthenticated,
+    upload.single('avatar'),
+    userAvatarController.update,
+    );
 
 export default usersRouter;
