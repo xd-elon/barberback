cad6884fb8b502e317b262a093df77eb17066c9f
xd-elon <alandevelope@gmail.com>
2021-03-20 19:57:07 -0300

    update routes  appointments

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,35 +1,14 @@
-import { parseISO } from 'date-fns';
 import { Router } from 'express';
 
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentServices';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
+import AppointmentsController from '../controllers/AppointmetsController';
+
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository()
+const appointmentsController = new AppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-//appointmentsRouter.get('/', async (request, response) => {
-//    const appointments = await appointmentsRepository.find();
-//
-//   return response.json(appointments);
-//});
-
-appointmentsRouter.post('/', async (request, response) => {
- 
-    const { provider_id, date } = request.body;
-
-    const parsedDate = parseISO(date);
-
-    const createAppointment = new CreateAppointmentService(appointmentsRepository);
-
-    const appointment = await createAppointment.execute({
-      provider_id, 
-      date: parsedDate
-    });
-
-    return response.json(appointment)
-});
+appointmentsRouter.post('/', appointmentsController.create);
 
 export default appointmentsRouter;
