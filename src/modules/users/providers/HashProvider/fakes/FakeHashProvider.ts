906a12cdf6b39ca561e333ec2ca013e23bbdfd12
xd-elon <alandevelope@gmail.com>
2021-03-25 16:39:37 -0300

    fake hashprovider test

---
+++ src/modules/users/providers/HashProvider/fakes/FakeHashProvider.ts
@@ -0,0 +1,13 @@
+import IHasheProvider from '../models/IHashedProvider';
+
+class FakeHashProvider implements IHasheProvider {
+    public async generateHash(payload: string): Promise<string> {
+        return payload
+    }
+
+    public async compareHash(payload: string, hashed: string): Promise<boolean> {
+        return payload == hashed;
+    }
+}
+
+export default FakeHashProvider;
